c26e0349752ffe44c77c8890623bbfa0b2a3f863
Andrew Uy <37019607+uy-andrew@users.noreply.github.com>
2019-12-12 22:16:55 +1100

    Add Angular 8 application to demo the multi-tab-detection node module.

---
+++ angular-app/src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { MultiTabDetection } from 'multi-tab-detection';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-app';
+  multiTabDetection: MultiTabDetection;
+  listOfEvents: string[];
+  timeOutSet: boolean;
+
+  constructor() {
+    this.initializeData();
+    this.initializeDetection();
+  }
+
+  initializeData() {
+    this.listOfEvents = [];
+    this.timeOutSet = false;
+  }
+
+  initializeDetection() {
+    this.multiTabDetection = new MultiTabDetection();
+
+    const thisInstance = this;
+    this.multiTabDetection.ExistingTabDetectedEvent.subscribe({
+      next: () => {
+        const dateTimeNow = new Date();
+        thisInstance.listOfEvents.push(`${dateTimeNow.toLocaleString()} - Existing Tab Detected`);
+
+        if (thisInstance.timeOutSet === false) {
+          thisInstance.timeOutSet = true;
+          setTimeout(() => {
+            thisInstance.timeOutSet = false;
+            console.log(`${thisInstance.multiTabDetection.NumberOfTabsOpened} tabs opened`);
+          }, 1000);
+        }
+
+      }
+    });
+
+    this.multiTabDetection.NewTabDetectedEvent.subscribe({
+      next: (totalNumberOfTabsOpened) => {
+        const dateTimeNow = new Date();
+        thisInstance.listOfEvents.push(`${dateTimeNow.toLocaleString()} - New Tab Detected. ` +
+          `totalNumberOfTabsOpened = ${totalNumberOfTabsOpened}`);
+      }
+    });
+  }
+}
